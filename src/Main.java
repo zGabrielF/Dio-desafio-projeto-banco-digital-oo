dea56da0fe8ec1f36799213c3ee89fd52e8a25a6
zGabrielF <myththeblack@gmail.com>
2022-04-20 17:34:02 -0300

    desafio dio banco digital com Orientação a Objeto

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente gabriel = new Cliente();
+        gabriel.setNome("Gabriel");
+
+        Conta cc = new ContaCorrente(gabriel);
+        Conta poupanca = new ContaPoupanca(gabriel);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
